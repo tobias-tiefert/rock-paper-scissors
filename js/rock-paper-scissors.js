6a6be4f8aedcbb827509c36f316d3ffe2a45375b
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2025-11-23 23:18:23 +0100

    Build the basic functionality
    I built the basisc functionality to play one round when the page loads.
    The computer take a choice, the user can take a choice via a prompt,
    the result is displayed in the console.
    To play another round you have to refresh the page.

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,73 @@
+/*
+
+getHumanrChoice
+
+prompt anzeigen, 
+inhalt als return Value laden
+
+
+
+
+*/
+
+const questionForUser = "What do you chose, rock, paper or scissors?"
+
+function getIntegerBetween1andN(n){
+    return Math.ceil( Math.random() * n)
+}
+
+
+function getComputerChoice(){
+    let choice = getIntegerBetween1andN(3);
+    switch(choice){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+function getHumanChoice(){
+    return prompt(questionForUser);
+};/*
+function getFirstLetterLowerCase(string){
+    let newString = string.toLowerCase();
+    return newString.slice(0,1);
+};
+*/
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
+console.log(computerChoice);
+
+
+
+function getResult(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log("It's a draw")
+    } else if (humanChoice === "rock"){
+        if (computerChoice === "scissors"){
+            console.log("You win")
+        } else if (computerChoice === "paper"){
+            console.log("The computer wins")
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "scissors"){
+            console.log("The computer wins")
+        } else if (computerChoice === "paper"){
+            console.log("You win")
+        }
+    } else if (humanChoice === "scissors"){
+        if (computerChoice === "rock"){
+            console.log("The computer wins")
+        } else if (computerChoice === "paper"){
+            console.log("You win")
+        }
+    }
+}
+getResult(humanChoice, computerChoice)
