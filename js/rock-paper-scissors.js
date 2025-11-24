a152f9dacb422205b779dc7a8b631384e0d17f8d
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2025-11-24 13:08:28 +0100

    Finish rounds, game and output for user
    Now the game plays 5 rounds, displays who won which round, which round
    it was and what the current scores are.
    At the end of the game it announces the winner of the game.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -10,7 +10,20 @@ inhalt als return Value laden
 
 */
 
-const questionForUser = "What do you chose, rock, paper or scissors?"
+const questionForUser = "What do you chose, rock, paper or scissors?";
+
+let humanScore = 0; 
+let computerScore = 0;
+
+function displayWinner(winner){
+    if (winner === "computer"){
+        console.log("The computer wins");
+        computerScore++;
+    } else if (winner === "human"){
+        console.log("You win");
+        humanScore++;
+    }
+}
 
 function getIntegerBetween1andN(n){
     return Math.ceil( Math.random() * n)
@@ -34,41 +47,55 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     return prompt(questionForUser);
-};/*
-function getFirstLetterLowerCase(string){
-    let newString = string.toLowerCase();
-    return newString.slice(0,1);
 };
-let computerChoice = "scissors";
-let humanChoice = "rock"
-*/
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
 
-console.log(humanChoice);
-console.log(computerChoice);
 
-function getResult(humanChoice, computerChoice){
-    if (humanChoice === computerChoice){
-        console.log("It's a draw")
-    } else if (humanChoice === "rock"){
-        if (computerChoice === "scissors"){
-            console.log("You win")
-        } else if (computerChoice === "paper"){
-            console.log("The computer wins")
-        }
-    } else if (humanChoice === "paper"){
-        if (computerChoice === "scissors"){
-            console.log("The computer wins")
-        } else if (computerChoice === "rock"){
-            console.log("You win")
-        }
-    } else if (humanChoice === "scissors"){
-        if (computerChoice === "rock"){
-            console.log("The computer wins")
-        } else if (computerChoice === "paper"){
-            console.log("You win")
+function playGame(){
+    humanScore = 0; 
+    computerScore = 0;
+
+    function playRound(){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === computerChoice){
+            console.log("It's a draw")
+        } else if (humanChoice === "rock"){
+            if (computerChoice === "scissors"){
+                displayWinner("human");
+            } else if (computerChoice === "paper"){
+                displayWinner("computer");
+            }
+        } else if (humanChoice === "paper"){
+            if (computerChoice === "scissors"){
+                displayWinner("computer");
+            } else if (computerChoice === "rock"){
+                displayWinner("human");
+            }
+        } else if (humanChoice === "scissors"){
+            if (computerChoice === "rock"){
+                displayWinner("computer");
+            } else if (computerChoice === "paper"){
+                displayWinner("human");
+            }
         }
     }
+
+    for(let i = 0; i < 5; i++){
+        playRound();
+        console.log("Your score: " + humanScore);
+        console.log("Computer's score: " + computerScore);
+        console.log("Rounds played: "+ (i+1));
+        console.log("------------------------------------")
+    };
+
+    if(humanScore > computerScore){
+        console.log("You won the game!")
+    } else if(humanScore < computerScore){
+        console.log("The computer won the game")
+    } else{
+        console.log("The game was a draw")
+    };
 }
-getResult(humanChoice, computerChoice)
+
+playGame();
